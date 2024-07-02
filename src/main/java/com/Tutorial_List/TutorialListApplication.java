4bd1932f11d0cfa7a6b5ceede27f73dea191debb
Jose Manuel Cruz Pacheco <cruzpachecojosemanuel@gmail.com>
2024-07-02 13:50:47 +0200

    first commit

---
+++ src/main/java/com/Tutorial_List/TutorialListApplication.java
@@ -0,0 +1,13 @@
+package com.Tutorial_List;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Tutorial_List/TutorialListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tutorial_List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorialListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
